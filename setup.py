9935702b8603e6660a2b0ce7591b5314f89021e9
jzerilli <jack.zerilli@gmail.com>
2021-01-22 17:00:11 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name="example_package",
+    version="0.0.1",
+    author="Example Author",
+    author_email="author@example.com",
+    description="A small example package",
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+)
